2f0c422db3d85bf2fbbe41f335a2697b2fa7e89f
welpodron <welpodron@gmail.com>
2023-02-11 22:54:07 +0300

    Базовая инициализация проекта

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+  },
+  webpack(config, option) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ru">
+      <Head>
+        <link
+          type="font/woff2"
+          href="/fonts/opensans.woff2"
+          rel="preload"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          type="font/woff2"
+          href="/fonts/opensanslight.woff2"
+          rel="preload"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          type="font/woff2"
+          href="/fonts/opensansbold.woff2"
+          rel="preload"
+          as="font"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
